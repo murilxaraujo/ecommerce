cc8b35ec6a1caa77716db2cdee1cce10e1696900
Murilo Araujo <muernandes@icloud.com>
2020-01-10 22:10:28 -0300

    create header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-import { Route } from "react-router-dom";
-
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/header/header.component";
 import ShopPage from "./pages/shoppage/shoppage.component";
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
